d7681ee4a5b65f125717ef9529df04d116f575b3
Rachel <124736890+JinChun5239@users.noreply.github.com>
2025-01-18 13:09:23 -0800

    Initial Commit

---
+++ Roominate/RoominateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoominateApp.swift
+//  Roominate
+//
+//  Created by Rachel Chiu on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct RoominateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RoominateTests/RoominateTests.swift
@@ -0,0 +1,16 @@
+//
+//  RoominateTests.swift
+//  RoominateTests
+//
+//  Created by Rachel Chiu on 2025-01-18.
+//
+
+import Testing
+
+struct RoominateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
