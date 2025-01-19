cc278722abb4e1cc1fb742561ba88f4bcd6d4afe
Rachel <124736890+JinChun5239@users.noreply.github.com>
2025-01-19 12:10:34 -0800

    improve login UI

--- Roominate/auth/LoginView.swift
+++ Roominate/auth/LoginView.swift
@@ -11,54 +11,63 @@ struct LoginView: View {
             // ContentView()
             RoomView()
         } else{
-            VStack(spacing: 20) {
-                Button(action: {
-                    goback=true;
-                }) {
-                    Text("Return")
-                }
-                Text("Login")
-                    .font(.largeTitle)
-                    .bold()
-                    .padding(.top, 40)
+            
+            ZStack {
                 
-                Spacer()
-                
-                // Username Field
-                TextField("Username", text: $viewModel.username)
-                    .padding()
-                //                .background(Color(UIColor.systemGray6))
-                    .cornerRadius(10)
-                //                .autocapitalization(.none)
-                
-                // Password Field
-                SecureField("Password", text: $viewModel.password)
-                    .padding()
-                //                .background(Color(UIColor.systemGray6))
-                    .cornerRadius(10)
-                
-                // Login Button
-                Button(action: {
-                    viewModel.handleLogin()
-                }) {
+                // Background colour
+                Color(.systemMint)
+                    .ignoresSafeArea()
+                VStack(spacing: 20) {
+                    Button(action: {
+                        goback=true;
+                    }) {
+                        Text("Return")
+                            .foregroundStyle(.white)
+                    }
                     Text("Login")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(.top, 40)
+                    
+                    Spacer()
+                    
+                    // Username Field
+                    TextField("Username", text: $viewModel.username)
                         .padding()
-                        .background(Color.blue)
+                    //                .background(Color(UIColor.systemGray6))
                         .cornerRadius(10)
+                    //                .autocapitalization(.none)
+                    
+                    // Password Field
+                    SecureField("Password", text: $viewModel.password)
+                        .padding()
+                    //                .background(Color(UIColor.systemGray6))
+                        .cornerRadius(10)
+                    
+                    // Login Button
+                    Button(action: {
+                        viewModel.handleLogin()
+                    }) {
+                        Text("Login")
+                            .font(.headline)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .foregroundStyle(.black)
+                            .background(.white)
+                    }
+                    
+                    Spacer()
                 }
-                
-                Spacer()
+                .padding()
+                .alert(isPresented: $viewModel.showAlert) {
+                    Alert(title: Text("Login"), message: Text(viewModel.alertMessage), dismissButton: .default(Text("OK")))
+                }
+                //        .fullScreenCover(isPresented: $viewModel.isAuthenticated) {
+                //            ContentView() // Replace with the view you want to show after successful login
+                //        }
             }
-            .padding()
-            .alert(isPresented: $viewModel.showAlert) {
-                Alert(title: Text("Login"), message: Text(viewModel.alertMessage), dismissButton: .default(Text("OK")))
-            }
-            //        .fullScreenCover(isPresented: $viewModel.isAuthenticated) {
-            //            ContentView() // Replace with the view you want to show after successful login
-            //        }
         }
     }
 }
--- Roominate/auth/Start.swift
+++ Roominate/auth/Start.swift
@@ -10,17 +10,30 @@ struct Start: View {
         }else if (signup) {
             SignUpView()
         } else {
-            Button(action: {
-                login=true;
-            }) {
-                Text("Login")
-                    .foregroundStyle(.black)
-            }
-            Button(action: {
-                signup=true;
-            }) {
-                Text("Signup")
-                    .foregroundStyle(.black)
+            
+            ZStack {
+                
+                // Background colour
+                Color(.systemMint)
+                    .ignoresSafeArea()
+                VStack {
+                    Button(action: {
+                        login=true;
+                    }) {
+                        Text("Login")
+                            .foregroundStyle(.black)
+                            .frame(width: 300, height: 80)
+                            .background(.white)
+                    }
+                    Button(action: {
+                        signup=true;
+                    }) {
+                        Text("Signup")
+                            .foregroundStyle(.black)
+                            .frame(width: 300, height: 80)
+                            .background(.white)
+                    }
+                }
             }
         
         }
@@ -28,5 +41,5 @@ struct Start: View {
 }
 
 #Preview {
-    ScheduleView()
+    Start()
 }
