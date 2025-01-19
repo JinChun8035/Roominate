65f32dafab41461fa3d217f3f3c32970f889f62c
Rachel <124736890+JinChun5239@users.noreply.github.com>
2025-01-19 10:45:53 -0800

    room view navs to content

--- Roominate/auth/RoomView.swift
+++ Roominate/auth/RoomView.swift
@@ -13,22 +13,27 @@ struct RoomView: View {
     
     @StateObject private var viewModel = RoomViewModel()
     
+    @State private var goback = false
     var body: some View {
-        Button("Create new room") {
-            viewModel.createRoom()
-            // TODO: nav to content view and
+        if (goback) {
             ContentView()
-        }
-        
-        Text("")
-        Text("")
-        Text("")
-        
-        TextField("ROOM CODE", text: $viewModel.roomCode)
-            .padding()
-            .border(.gray)
-        Button("Join existing room") {
-            // action
+        } else {
+            Button("Create new room") {
+                viewModel.createRoom()
+                // TODO: nav to content view and
+                goback = true
+            }
+            
+            Text("")
+            Text("")
+            Text("")
+            
+            TextField("ROOM CODE", text: $viewModel.roomCode)
+                .padding()
+                .border(.gray)
+            Button("Join existing room") {
+                // action
+            }
         }
     }
 }
