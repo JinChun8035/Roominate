1dc2429697836bbf74baf13c33f3c5df8ad429bd
Rachel <124736890+JinChun5239@users.noreply.github.com>
2025-01-18 16:17:56 -0800

    lo-fi prototype of main view

--- Roominate/MainView.swift
+++ Roominate/MainView.swift
@@ -1,21 +1,57 @@
-//
-//  ContentView.swift
-//  Roominate
-//
-//  Created by Rachel Chiu on 2025-01-18.
-//
-
 import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Color(red: 229.0, green: 236.0, blue: 255.0)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                HStack {
+                    // Logo
+                    Image("logoPlaceholder")
+                        .resizable()
+                        .cornerRadius(15)
+                        .aspectRatio(contentMode: .fit)
+                    // Add roommate
+                    RoundedRectangle(cornerRadius: 3)
+                        .frame(width: 50.0, height: 50.0)
+                }
+                
+                // Profile picture
+                RoundedRectangle(cornerRadius: 3)
+                    .frame(width: 200, height: 200)
+                Text("name")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.black)
+                Spacer()
+                
+                // Instant alerts
+                HStack {
+                    // Lower the volume
+                    Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
+                        /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                        /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
+                    }
+                    
+                    // Custom
+                    
+                }
+                
+                // Navigation
+                RoundedRectangle(cornerRadius: 3)
+                    .frame(height: 80.0)
+                RoundedRectangle(cornerRadius: 3)
+                    .frame(height: 80.0)
+                RoundedRectangle(cornerRadius: 3)
+                    .frame(height: 80.0)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
